88d25300f64181eb272394ad0d761149d87c1858
Luca Bonavita <mindrones@users.noreply.github.com>
2019-10-14 13:14:42 +0100

    more npm aliases, revert to cat for short docs

--- .bash_profile_modules/.alias_npm.sh
+++ .bash_profile_modules/.alias_npm.sh
@@ -2,8 +2,13 @@
 
 # dev
 
+alias cpj='cat package.json'
+alias no='npm outdated'
+alias nh='npm home'
+alias ni='npm install'
 alias nr='npm run'
 alias nrd='npm run dev'
 alias nrs='npm run start'
 alias nrt='npm run test'
-alias cpj='cat package.json'
+alias ns='npm search'
+alias nu='npm uninstall'
